6c84f49e4573ea65a604c603fe9c1bae44151e56
LaihoE <80683769+LaihoE@users.noreply.github.com>
2024-07-07 16:38:22 +0300

    update protos (#191)

---
+++ src/csgoproto/README.md
@@ -0,0 +1,2 @@
+git clone https://github.com/SteamDatabase/Protobufs
+cargo run
--- src/csgoproto/src/lib.rs
+++ src/csgoproto/src/lib.rs
@@ -1,22 +1,10 @@
-// @generated
-extern crate protobuf;
-
-pub mod cs_gameevents;
-pub mod cs_usercmd;
 pub mod cstrike15_gcmessages;
 pub mod cstrike15_usermessages;
 pub mod demo;
-pub mod econ_gcmessages;
 pub mod engine_gcmessages;
-pub mod enums_clientserver;
-pub mod fatdemo;
-pub mod gameevents;
 pub mod gcsdk_gcmessages;
 pub mod netmessages;
 pub mod network_connection;
 pub mod networkbasetypes;
-pub mod networksystem_protomessages;
 pub mod steammessages;
-pub mod te;
-pub mod usercmd;
 pub mod usermessages;
