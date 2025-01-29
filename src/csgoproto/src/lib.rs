4de30b8811be008374ff308159c39737864f4235
LaihoE <80683769+LaihoE@users.noreply.github.com>
2025-01-29 22:48:03 +0200

    fix unk cmd (#240)

--- src/csgoproto/src/lib.rs
+++ src/csgoproto/src/lib.rs
@@ -1,12 +1,13 @@
-pub mod cs_usercmd;
+
 pub mod cstrike15_gcmessages;
 pub mod cstrike15_usermessages;
 pub mod demo;
+pub mod cs_usercmd;
 pub mod engine_gcmessages;
 pub mod gcsdk_gcmessages;
 pub mod netmessages;
 pub mod network_connection;
 pub mod networkbasetypes;
 pub mod steammessages;
-pub mod usercmd;
 pub mod usermessages;
+pub mod usercmd;
