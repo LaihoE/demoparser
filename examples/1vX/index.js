93cf9a4ca851f251908339e1ffa2a684b4b0b241
Eric Reinke <35472210+ericreinke@users.noreply.github.com>
2025-04-09 09:59:45 -0400

    Replaced team ints with string literals (#268)

--- examples/1vX/index.js
+++ examples/1vX/index.js
@@ -13,11 +13,11 @@ function find_if_1vx(deaths, round_idx, round_ends, tickData, X){
             let ctAlive = tickData_slice.filter(t => t.team_name == "CT" && t.is_alive == true)
             let tAlive = tickData_slice.filter(t => t.team_name == "TERRORIST" && t.is_alive == true)
             // 3 = CT
-            if (ctAlive.length == 1 && tAlive.length == X && round_ends[round_idx].winner == 3){
+            if (ctAlive.length == 1 && tAlive.length == X && round_ends[round_idx].winner == "CT"){
                 return ctAlive[0].name
             }
             // 2 = T
-            if (tAlive.length == 1 && ctAlive.length == X && round_ends[round_idx].winner == 2){
+            if (tAlive.length == 1 && ctAlive.length == X && round_ends[round_idx].winner == "T"){
                 return tAlive[0].name
             }
         }
